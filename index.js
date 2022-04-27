ecd7728e3c90d02f649aad9f1e92ab4b0b381a98
Arif <aharif281@gmail.com>
2022-04-27 20:23:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,140 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const jwt =require('jsonwebtoken');
+const cors=require('cors');
+const port=process.env.PORT || 5000;
+// mongodb
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const ObjectId=require('mongodb').ObjectId;
+app.use(cors());
+app.use(express.json());
+
+
+// jwt veryfy
+function verifyJwt(req,res,next){
+   const authheader=req.headers.authorization;
+   if(!authheader){
+    return res.status(401).send({message:'unauthorize access'});
+  }
+  const token =authheader.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded) =>{
+      if(err){
+        return res.status(403).send({message:'Forbindd access'});
+      }
+      console.log('decoded',decoded);
+      req.decoded=decoded;
+      next();
+    })
+  //  console.log('inside verify',authheader);
+  
+
+  
+}
+
+
+// products
+// KK4fVHwuIlZxydK7
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://products:KK4fVHwuIlZxydK7@cluster0.daver.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+     try{
+      await client.connect();
+      const userCollection = client.db("Foodexpress").collection("user");
+      const userOrder = client.db("Foodexpress").collection("order");
+
+// jwt token
+
+      app.post('/login',async(req,res)=>{
+        const user=req.body;
+        const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+          expiresIn: '1d'
+        });
+        res.send({accessToken});
+      })
+       
+
+// service
+
+      app.get('/service',async(req,res) => {
+        const query= {} ;
+        const cursor= userCollection.find(query);
+        const users=await cursor.toArray();
+        res.send(users)
+      })
+      app.get('/service/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:ObjectId(id)};
+        const result=await userCollection.findOne(query);
+        res.send(result); 
+      })
+
+      // POST data
+    app.post('/service',async(req,res) =>{
+      const newUser =req.body;
+      const result = await userCollection.insertOne(newUser);
+     console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      res.send(result);
+
+    });
+      // delete data
+    app.delete('/service/:id', async(req,res) =>{
+      const id=req.params.id;
+      const query={_id:ObjectId(id)};
+      const result= await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // order
+    app.post('/order',async(req,res) =>{
+      const newUser =req.body;
+      const result = await userOrder.insertOne(newUser);
+     console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      res.send(result);
+    });
+
+    app.get('/order',verifyJwt, async(req,res) => {
+      const decodedEmail=req.decoded.email;
+      const email=req.query.email;
+      if(email === decodedEmail){
+      const query={email:email};
+      const cursor= userOrder.find(query);
+      const users=await cursor.toArray();
+      res.send(users)
+      }
+      else{
+        res.status(403).send({message:'forbidden acccess'})
+      }
+    })
+
+
+  
+
+
+     }
+     finally{
+      //  awite client .close();
+     }
+}
+run().catch(console.dir);
+
+
+
+
+
+  
+app.get('/',(req,res) =>{
+    res.send('hello nodemon ami tomake install lorci node js')
+})
+
+app.get('/services',(req,res) =>{
+    res.send('all service')
+})
+
+
+app.listen(port,() =>{
+    console.log('listen some thing',port)
+})
